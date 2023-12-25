495b946295d68f2276c8109f44940dcedfbf2a13
denis <sirazov1508@gmail.com>
2023-12-26 00:35:43 +0300

    done with 6th

--- task6/task.py
+++ task6/task.py
@@ -1,53 +1,22 @@
 import json
 import numpy as np
 
-def calc_list_of_ranks(r_str: str, tmpl: dict) -> list[int]:
-    r = json.loads(r_str)
-    r_list = [0] * len(tmpl)
+def calculate_desp_similarity(first_order, second_order, third_order):
+    first_order = json.loads(first_order)
+    second_order = json.loads(second_order)
+    third_order = json.loads(third_order)
 
-    for i, el in enumerate(r):
-        if isinstance(el, list):
-            for sub_el in el:
-                r_list[tmpl[sub_el]] = i + 1
-        else:
-            r_list[tmpl[el]] = i + 1
+    n = len(first_order)
+    res = np.zeros(n)
+    avg = np.sum([int(first_order[i][1]) + int(second_order[i][1]) + int(third_order[i][1]) for i in range(n)]) / n
 
-    return r_list
+    desp = np.sum([(el - avg) ** 2 for el in res]) / (2 * 9 * (n ** 3 - n) / (12 * (n - 1)))
 
-def create_ranking_template(template_str: str) -> dict:
-    template = dict()
-    count = 0
-
-    for element in json.loads(template_str):
-        if isinstance(element, list):
-            for sub_element in element:
-                template[sub_element] = count
-                count += 1
-        else:
-            template[element] = count
-            count += 1
-
-    return template
-
-def calculate_D_Dmax(matrix: np.ndarray) -> float:
-    sum_matrix = np.sum(matrix, axis=0)
-    D = np.var(sum_matrix) * matrix.shape[0] / (matrix.shape[0] - 1)
-    D_max = (matrix.shape[1] ** 2) * ((matrix.shape[0] ** 3 - matrix.shape[0]) / 12) / (matrix.shape[0] - 1)
-    
-    return D / D_max
-
-def task(*rankings) -> str:
-    experts_count = len(rankings)
-    template = create_ranking_template(rankings[0])
-    matrix = np.array([calc_list_of_ranks(r_str, template) for r_str in rankings])
-    
-    result = calculate_D_Dmax(matrix)
-    
-    return format(result, ".2f")
+    return desp
 
 if __name__ == "__main__":
-    A = '["1", ["2", "3"], "4", ["5", "6", "7"], "8", "9", "10"]'
-    B = '[["1", "2"], ["3", "4", "5"], "6", "7", "9", ["8", "10"]]'
-    C = '["3", ["1", "4"], "2", "6", ["5", "7", "8"], ["9", "10"]]'
+    print(calculate_desp_similarity(
+        '["O1","O2","O3"]',
+        '["O1","O3","O2"]',
+        '["O1","O3","O2"]'))
 
-    print(task(A, B, C))
