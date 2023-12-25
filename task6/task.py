4feb66007eae111a403d16e9818647af48af9e12
denis <sirazov1508@gmail.com>
2023-12-26 01:35:08 +0300

    fix

--- task6/task.py
+++ task6/task.py
@@ -7,10 +7,10 @@ def task(first_order, second_order, third_order):
     third_order = json.loads(third_order)
 
     n = len(first_order)
-    res = np.zeros(n)
-    avg = np.sum([int(first_order[i][1]) + int(second_order[i][1]) + int(third_order[i][1]) for i in range(n)]) / n
+    res = np.array([int(first_order[i][1]) + int(second_order[i][1]) + int(third_order[i][1]) for i in range(n)])
+    avg = np.mean(res)
 
-    desp = np.sum([(el - avg) ** 2 for el in res]) / (2 * 9 * (n ** 3 - n) / (12 * (n - 1)))
+    desp = np.sum((res - avg) ** 2) / (2 * 9 * (n ** 3 - n) / (12 * (n - 1)))
 
     return desp
 
@@ -19,4 +19,3 @@ if __name__ == "__main__":
         '["O1","O2","O3"]',
         '["O1","O3","O2"]',
         '["O1","O3","O2"]'))
-
